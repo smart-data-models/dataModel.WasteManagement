f789fb9dd9ac8fb2696357c4a8f5cf4724aff9a6
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 13:53:40 +0200

    test of SQL export

--- WasteContainerIsle/schema.sql
+++ WasteContainerIsle/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model WasteContainerIsle of the subject dataModel.WasteManagement for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE WasteContainerIsle_type AS ENUM ('WasteContainerIsle');
-CREATE TABLE WasteContainerIsle (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, features JSON, insertHolesNumber NUMERIC, name TEXT, owner JSON, refWasteContainer JSON, source TEXT, type WasteContainerIsle_type);
+CREATE TABLE WasteContainerIsle (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, features JSON, id TEXT PRIMARY KEY, insertHolesNumber NUMERIC, location JSON, name TEXT, owner JSON, refWasteContainer JSON, seeAlso JSON, source TEXT, type WasteContainerIsle_type);
