722f69f4c5182c7fbd3fb875f7463a573ae24ffc
Alberto Abella <alberto.abella@fiware.org>
2024-05-24 17:19:52 +0200

    created example of code

--- WasteContainerIsle/code/code_for_using_dataModel.WasteManagement_WasteContainerIsle.py
+++ WasteContainerIsle/code/code_for_using_dataModel.WasteManagement_WasteContainerIsle.py
@@ -24,19 +24,24 @@
 #         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
 #         
 #         # now the python code you can use to insert some value in the context broker according to the data model
+#         # Version Warning! 
+#         # This code is designed to work with the version 0.8 of pysmartdatamodels or later
+#         # to work with earlier version you need to replace the import instruction for
+#         # from pysmartdatamodels import pysmartdatamodels as sdm
 #         
-from pysmartdatamodels import pysmartdatamodels as sdm
+#         
+import pysmartdatamodels as sdm
 import subprocess
 serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost. Edit to match your configuration
 dataModel = "WasteContainerIsle"
 subject = "dataModel.WasteManagement"
-features = {'type': 'Property', 'value': ['underground']}
+features = ['underground']
 attribute = "features"
 value = features
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-refWasteContainer = {'type': 'Relationship', 'object': ['urn:ngsi-ld:WasteContainer:wastecontainer:Fleming:12a', 'urn:ngsi-ld:WasteContainer:wastecontainer:Fleming:12b']}
+refWasteContainer = ['urn:ngsi-ld:WasteContainer:wastecontainer:Fleming:12a', 'urn:ngsi-ld:WasteContainer:wastecontainer:Fleming:12b']
 attribute = "refWasteContainer"
 value = refWasteContainer
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
